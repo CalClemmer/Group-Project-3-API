1b985c84ce4c194858b471a8b2bd9181ff5c2658
CalClemmer <cal1296@yahoo.com>
2021-09-29 16:07:36 -0700

    Add: Steam Reviews to database

--- playground.js
+++ playground.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const { Videogame } = require("./models");
+const axios = require("axios");
 
-var json = require("./models/api_data/videogameData.json");
+const videogameDatajson = require("./models/api_data/videogameData.json");
+const appIDsteamjson = require("./models/api_data/appIDsteam.json");
 
-// Videogame.insertMany(json, function (err, result) {
+// inserts videogame data into mongodb
+// Videogame.insertMany(videoGameDatajson, function (err, result) {
 //   if (err) {
 //     // handle error
 //   } else {
@@ -12,28 +15,154 @@ var json = require("./models/api_data/videogameData.json");
 //   }
 // });
 
-(async function viewAllGames() {
+console.log("Console log", appIDsteamjson["applist"]["apps"].length);
+const appIdArr = appIDsteamjson["applist"]["apps"];
+
+// console.log(appIdArr[0]["appid"]); // 5
+
+// returns
+// [
+//   { appid: 5, name: 'Dedicated Server' },
+//   { appid: 7, name: 'Steam Client' },
+//   { appid: 8, name: 'winui2' },
+//   ...
+// ]
+
+async function getStoreData(id) {
+  var data = "";
+  var config = {
+    method: "get",
+    url: `http://store.steampowered.com/appreviews/${id}?json=1&num_per_page=0`,
+    headers: {},
+    data: data,
+  };
+  let ratio;
+
+  try {
+    const response = await axios(config);
+    let totalReviews = JSON.stringify(
+      response.data.query_summary.total_reviews
+    );
+    let positiveReviews = JSON.stringify(
+      response.data.query_summary.total_positive
+    );
+    let percent = positiveReviews / totalReviews;
+    ratio = Math.round(percent * 1000) / 10;
+
+    return ratio;
+  } catch (error) {
+    console.log(error);
+  }
+  // try {
+  //   let ratio;
+  //   axios(config).then(function (response) {
+  //     let totalReviews = JSON.stringify(
+  //       response.data.query_summary.total_reviews
+  //     );
+  //     let positiveReviews = JSON.stringify(
+  //       response.data.query_summary.total_positive
+  //     );
+  //     let percent = positiveReviews / totalReviews;
+  //     ratio = Math.round(percent * 1000) / 10;
+  //     console.log("ratio", ratio);
+  //   });
+
+  //   return 2;
+  // } catch (error) {
+  //   console.log(error);
+  // }
+
+  //   .catch(function (error) {
+  //     console.log(error, "It no work");
+  //   });
+}
+
+async function addSteamReviews() {
+  for (let i = 0; i < videogameDatajson.length; i++) {
+    let name = videogameDatajson[i]["name"];
+    let gameData = await Videogame.find({ name: name });
+    let steamID = gameData[0]["steamID"];
+    if (steamID) {
+      let steamReview = await getStoreData(steamID);
+      if (steamReview) {
+        updateSteamReview(steamID, steamReview);
+        console.log("steamID", steamID);
+      }
+    }
+  }
+  console.log("We done!!!!");
+}
+
+// addSteamReviews();
+// adds all steam reviews to games; takes a minute, super satisfying
+
+function addSteamIDs() {
+  for (let i = 0; i < appIdArr.length; i++) {
+    let name = appIdArr[i]["name"];
+    let steamId = appIdArr[i]["appid"];
+    updateOneNameId(name, steamId);
+  }
+}
+
+// Takes about 30 seconds to run
+// addSteamIDs();
+
+// this works, now I need to try to automate it
+async function updateOneNameId(name, steamID) {
+  let filter = { name: name };
+  let update = { steamID: steamID };
+  try {
+    await Videogame.findOneAndUpdate(filter, update);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updateSteamReview(steamID, steamReview) {
+  let filter = { steamID: steamID };
+  let update = { steamReview: steamReview };
+  try {
+    await Videogame.findOneAndUpdate(filter, update);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function viewAllGames() {
   try {
     let allGames = await Videogame.find({});
     console.log(allGames, "You did say ALL games...");
   } catch (error) {
     console.log(error);
   }
-})();
+}
 
-// (async function makeNewProduct() {
-//   let newProduct = await Product.create({
-//     name: "Play Doh",
-//     price: "6",
-//     companyName: "D'oh",
-//     ratings: "42",
-//     condition: "slighty chewed",
-//     description: "Eat the Play Doh.",
-//   });
-//   console.log(newProduct);
-// })();
+async function viewTop(n) {
+  let trending = await Videogame.find({})
+    .sort({ total_rating_count: -1 })
+    .limit(n);
+  // const res = await Customer.find({}).sort({ name: 1 }).limit(1);
 
-// name: { type: String, required: true },
-// genre: { type: String },
-// imageURL: { type: String },
-// desc: { type: String },
+  try {
+    console.log(trending);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function viewHundreds() {
+  try {
+    let allGames = await Videogame.find({ steamReview: 100 });
+    console.log(allGames, "100? Really? Hm...");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// viewHundreds();
+
+// updateOne();
+
+// viewTop(30);
+
+// viewAllGames();
